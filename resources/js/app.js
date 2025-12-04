34915b494d74db565916e9d6737d702992095747
Muhammad-Umair10 <rajputumair0301@gmail.com>
2025-12-04 21:36:29 +0500

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,3 @@
+import '../../public/js/bootstrap';
+
+
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/shop', function(){
+    return view('shop');
+})->name('shop');
+
+Route::get('/about-us', function(){
+    return view('about-us');
+})->name('About');
+
+Route::get('/cart', function(){
+    return view('cart');
+})->name('cart');
+
+Route::get('/services', function(){
+    return view('services');
+})->name('services');
+
+Route::get('/blogs', function(){
+    return view('blogs');
+})->name('blogs');
+
+Route::get('/contact', function(){
+    return view('contact');
+})->name('contact');
+
+Route::get('/register', function(){
+    return view('register');
+})->name('register');
+
+Route::get('/login',function(){
+    return view('login');
+})->name('login');
